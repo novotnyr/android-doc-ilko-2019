db5a82ec9ada1ce04fa35ea530d8ece7a962818d
Robert Novotny <novotnr0@gmail.com>
2019-04-11 16:46:57 +0200

    Pridany zdielany ViewModel pre fragmenty
    Master vysiela zmeny do ViewModelu, Detail ich observuje a aktualizuje
    TextView

--- app/src/main/java/com/github/novotnyr/android/ilko/DetailFragment.java
+++ app/src/main/java/com/github/novotnyr/android/ilko/DetailFragment.java
@@ -4,10 +4,13 @@ package com.github.novotnyr.android.ilko;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 /**
@@ -15,6 +18,7 @@ import android.view.ViewGroup;
  */
 public class DetailFragment extends Fragment {
 
+    private TextView temperatureTextView;
 
     public DetailFragment() {
         // Required empty public constructor
@@ -24,8 +28,20 @@ public class DetailFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detail, container, false);
+        View root = inflater.inflate(R.layout.fragment_detail, container, false);
+
+        temperatureTextView = root.findViewById(R.id.temperatureTextView);
+
+        final TemperatureViewModel temperatureViewModel = ViewModelProviders.of(getActivity()).get(TemperatureViewModel.class);
+        temperatureViewModel.getSelectedCity().observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(String city) {
+                int temperature = Math.abs(city.hashCode()) % 32;
+                temperatureTextView.setText("" + temperature);
+            }
+        });
+
+        return root;
     }
 
 }
