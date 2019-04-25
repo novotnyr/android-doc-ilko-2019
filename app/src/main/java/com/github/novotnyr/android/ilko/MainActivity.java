8ad55aec908c302c81510002f105b68b913a080d
Robert Novotny <novotnr0@gmail.com>
2019-04-25 22:14:11 +0200

    Pridane prepinanie fragmentov v hlavnej aktivite

--- app/src/main/java/com/github/novotnyr/android/ilko/MainActivity.java
+++ app/src/main/java/com/github/novotnyr/android/ilko/MainActivity.java
@@ -1,6 +1,8 @@
 package com.github.novotnyr.android.ilko;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
 
 import android.os.Bundle;
 
@@ -10,5 +12,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (isSinglePanelMode()) {
+            final TemperatureViewModel temperatureViewModel = ViewModelProviders.of(this).get(TemperatureViewModel.class);
+            temperatureViewModel.getSelectedCity().observe(this, new Observer<String>() {
+                @Override
+                public void onChanged(String city) {
+                    MainActivity.this.onCityChanged(city);
+                }
+            });
+
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.activity_main, new MasterFragment())
+                    .commit();
+        }
+    }
+
+    private void onCityChanged(String city) {
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.activity_main, new DetailFragment())
+                .addToBackStack(null)
+                .commit();
+    }
+
+    private boolean isSinglePanelMode() {
+        return findViewById(R.id.activity_main) != null;
     }
 }
